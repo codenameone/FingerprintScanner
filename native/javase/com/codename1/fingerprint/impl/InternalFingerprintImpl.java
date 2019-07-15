3e8e31f324d95f21277d224a0098518547f54d44
Steve Hannah <steve@weblite.ca>
2019-07-15 06:22:40 -0700

    Added NSFaceIDUsageDescription build hint automatically, via simulator.

--- native/javase/com/codename1/fingerprint/impl/InternalFingerprintImpl.java
+++ native/javase/com/codename1/fingerprint/impl/InternalFingerprintImpl.java
@@ -1,14 +1,41 @@
 package com.codename1.fingerprint.impl;
 
+import com.codename1.ui.Display;
+import java.util.Map;
+
 public class InternalFingerprintImpl implements com.codename1.fingerprint.impl.InternalFingerprint{
+    
+    
+    private boolean faceIDUsageDescriptionChecked;
+    
+    private void checkFaceIDUsageDescription() {
+        if (!faceIDUsageDescriptionChecked) {
+            faceIDUsageDescriptionChecked = true;
+            
+            Map<String, String> m = Display.getInstance().getProjectBuildHints();
+            if(m != null) {
+                if(!m.containsKey("ios.NSFaceIDUsageDescription")) {
+                    Display.getInstance().setProjectBuildHint("ios.NSFaceIDUsageDescription", "Some functionality of the application requires FaceID functionality");
+                }
+            }
+        }
+    }
+    
+    private void installBuildHints() {
+        checkFaceIDUsageDescription();
+    }
+    
     public boolean isAvailable() {
+        installBuildHints();
         return false;
     }
 
     public void scan(String reason) {
+        installBuildHints();
     }
 
     public boolean isSupported() {
+        installBuildHints();
         return false;
     }
 
